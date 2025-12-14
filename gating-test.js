7b2efd4ab27bcad98c838ed7cd8987c4bddc7c75
demian7575 <168333310+demian7575@users.noreply.github.com>
2025-12-15 00:31:47 +0900

    feat: Gating test

---
+++ gating-test.js
@@ -0,0 +1,13 @@
+// Gating test
+// Test ECS worker
+
+/**
+ * Gating test
+ * Test ECS worker
+ */
+function gatingtest() {
+  // TODO: Implement Test ECS worker
+  return 'Not implemented';
+}
+
+export default gatingtest;
