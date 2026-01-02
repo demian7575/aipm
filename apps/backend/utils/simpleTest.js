496d839f5ce27283f61635d65bc7df6df3e3b7ff
EC2 User <ec2-user@aipm.local>
2026-01-02 10:20:44 +0000

    feat: Test Code Generation

---
+++ apps/backend/utils/simpleTest.js
@@ -0,0 +1,11 @@
+/**
+ * Simple test function
+ * @param {*} actual - Actual value
+ * @param {*} expected - Expected value
+ * @returns {boolean} True if values match
+ */
+function simpleTest(actual, expected) {
+  return actual === expected;
+}
+
+export { simpleTest };
