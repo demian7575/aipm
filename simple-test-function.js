5c5edd59550a54331f6e32b0914e872b7ca8b15f
EC2 User <ec2-user@aipm.local>
2026-01-03 13:40:54 +0000

    Enhance simple test function with validation capability

--- simple-test-function.js
+++ simple-test-function.js
@@ -1,9 +1,14 @@
 function simpleTest() {
-  return "Simple test function executed successfully";
+  return "Simple test function executed with validation";
 }
 
 function runSimpleTest() {
   return simpleTest();
 }
 
-module.exports = { simpleTest, runSimpleTest };
+function validateSimpleTest() {
+  const result = simpleTest();
+  return result.includes("executed");
+}
+
+module.exports = { simpleTest, runSimpleTest, validateSimpleTest };
