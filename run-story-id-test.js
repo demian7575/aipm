12f88615bdcc81f05497e9a167d6533e33b169f2
Junkye Bae <junkye.bae@ericsson.com>
2026-01-20 23:42:49 +0900

    Remove outdated test files and scripts

--- run-story-id-test.js
+++
@@ -1,6 +0,0 @@
-import { testStoryIdDisplay } from './test-story-id-display.js';
-
-testStoryIdDisplay().then(result => {
-  console.log('Test Result:', JSON.stringify(result, null, 2));
-  process.exit(result.success ? 0 : 1);
-});
