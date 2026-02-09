2067efebdceb385a21a85b98f161b89da9905cce
Junkye Bae <junkye.bae@ericsson.com>
2026-02-09 18:05:36 +0900

    fix: add ACCEPTANCE_TESTS_TABLE env var to backend service

--- scripts/deploy-to-environment.sh
+++ scripts/deploy-to-environment.sh
@@ -144,9 +144,11 @@ cd aipm
 sudo sed -i '/^Environment=DEPLOY_VERSION=/d' /etc/systemd/system/$SERVICE_NAME
 sudo sed -i '/^Environment=COMMIT_HASH=/d' /etc/systemd/system/$SERVICE_NAME
 sudo sed -i '/^Environment=PR_NUMBER=/d' /etc/systemd/system/$SERVICE_NAME
+sudo sed -i '/^Environment=ACCEPTANCE_TESTS_TABLE=/d' /etc/systemd/system/$SERVICE_NAME
 sudo sed -i '/^\[Service\]/a Environment=DEPLOY_VERSION=$DEPLOY_VERSION' /etc/systemd/system/$SERVICE_NAME
 sudo sed -i '/^\[Service\]/a Environment=COMMIT_HASH=$COMMIT_HASH' /etc/systemd/system/$SERVICE_NAME
 sudo sed -i '/^\[Service\]/a Environment=PR_NUMBER=$PR_NUMBER' /etc/systemd/system/$SERVICE_NAME
+sudo sed -i '/^\[Service\]/a Environment=ACCEPTANCE_TESTS_TABLE=$TESTS_TABLE' /etc/systemd/system/$SERVICE_NAME
 
 sudo systemctl daemon-reload
 EOF
